b72b9907246476109f52156c46ece8748268a068
Kadrizzle <lanekade8@gmail.com>
2024-02-27 11:27:58 -0600

    Now have a button that links to reservations page!

--- Selu383.SP24.Web/src/mainLayout/MainLayout.tsx
+++ Selu383.SP24.Web/src/mainLayout/MainLayout.tsx
@@ -1,21 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
-export function MainLayout (){
-    
-    return(
-        <>
-            <div className='navigation'>
-                <nav>
-                    <button
-                    
-                    > 
-                    swag 
-                    </button>
-                    <p> Huh </p>
-                </nav>
-            </div>
-            <hr></hr>
-            <Outlet/>
-        </>
-    );
+export function MainLayout() {
+  return (
+    <>
+      <div className="navigation">
+        <nav>
+          <button>swag</button>
+          <Link to="/reservations">Cick here to go to reservations</Link>
+        </nav>
+      </div>
+      <hr></hr>
+      <Outlet />
+    </>
+  );
 }
