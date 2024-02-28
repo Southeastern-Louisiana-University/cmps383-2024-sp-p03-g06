21a5e4f10ebe52a75854b6f137ddd2b246f92c02
Kadrizzle <lanekade8@gmail.com>
2024-02-27 19:24:59 -0600

    Adding a rooms class in the back end

--- Selu383.SP24.Api/Features/Hotels/HotelDto.cs
+++ Selu383.SP24.Api/Features/Hotels/HotelDto.cs
@@ -4,9 +4,9 @@ public class HotelDto
 {
     public int Id { get; set; }
 
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
-    public string Address { get; set; }
+    public string? Address { get; set; }
 
     public int? ManagerId { get; set; }
 }
