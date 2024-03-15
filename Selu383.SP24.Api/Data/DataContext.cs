135132bb262a1080678ec7f6d685bdf214fe3b8a
Kadrizzle <lanekade8@gmail.com>
2024-03-14 20:27:53 -0500

    please work again

--- Selu383.SP24.Api/Data/DataContext.cs
+++ Selu383.SP24.Api/Data/DataContext.cs
@@ -19,8 +19,8 @@ public class DataContext : IdentityDbContext<User, Role, int, IdentityUserClaim<
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<City>().HasData(new City { Id = 1, Location = "Baton Rouge" });
-        modelBuilder.Entity<City>().HasData(new City { Id = 2, Location = "New Orleans" });
+        modelBuilder.Entity<City>().HasData(new City { Location = "Baton Rouge" });
+        modelBuilder.Entity<City>().HasData(new City { Location = "New Orleans" });
 
 
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
