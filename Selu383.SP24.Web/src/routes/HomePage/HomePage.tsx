8ad578bff94ff958f1ab7ec9c190307c54784457
Kadrizzle <lanekade8@gmail.com>
2024-04-09 18:51:42 -0500

    slight error changes

--- Selu383.SP24.Web/src/routes/HomePage/HomePage.tsx
+++ Selu383.SP24.Web/src/routes/HomePage/HomePage.tsx
@@ -1,70 +1,55 @@
-import './HomePage.css';
+import "./HomePage.css";
 
 import { AppBar, ThemeProvider, Toolbar, createTheme } from "@mui/material";
-import { Container } from '@mantine/core';
+import { Container } from "@mantine/core";
 import { useState } from "react";
-import { addDays } from 'date-fns';
-import { DateRange } from 'react-day-picker';
-import { Link } from 'react-router-dom';
 
-
-const pastMonth = new Date(2024, 1, 1);
-
+import { Link } from "react-router-dom";
 
 function HomePage() {
-
-
-  const defaultSelected: DateRange = {
-      from: pastMonth,
-      to: addDays(pastMonth, 4)
-    };
-
-  const [range] = useState<DateRange | undefined>(defaultSelected);
-
-  if (range?.from) {
-    if (!range.to) {
-    } else if (range.to) {
-    }
-  }
-
   const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <>
       <ThemeProvider theme={darkTheme}>
         <Container>
-            <AppBar position="static" className="search-bar">
-              <Toolbar>
-                <label htmlFor="search">Search Destination</label>
-                <input id="search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value ?? "")}></input>
-                <Link 
-                  onClick={(e) => (!searchTerm ? e.preventDefault() : null)}
-                  to={`/find-city?searchTerm=${encodeURIComponent(searchTerm)}&start=now`}
-                  aria-disabled={!searchTerm}
-                >
-                  Search
-                </Link>
-                {/* <Box sx={{ flexGrow: 1 }} />
+          <AppBar position="static" className="search-bar">
+            <Toolbar>
+              <label htmlFor="search">Search Destination</label>
+              <input
+                id="search"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value ?? "")}
+              ></input>
+              <Link
+                onClick={(e) => (!searchTerm ? e.preventDefault() : null)}
+                to={`/find-city?searchTerm=${encodeURIComponent(
+                  searchTerm
+                )}&start=now`}
+                aria-disabled={!searchTerm}
+              >
+                Search
+              </Link>
+              {/* <Box sx={{ flexGrow: 1 }} />
                 <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
                 </Box> */}
-              </Toolbar>
-            </AppBar>
+            </Toolbar>
+          </AppBar>
         </Container>
-      </ThemeProvider> 
+      </ThemeProvider>
     </>
   );
 }
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'light',
+    mode: "light",
     primary: {
-      main: '#FFFFFF',
-      
+      main: "#FFFFFF",
     },
     secondary: {
-      main: '#DEDEDE'
-    }
+      main: "#DEDEDE",
+    },
   },
 });
 
