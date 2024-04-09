8070e2163e56987bbc4006af8a0cb1d27c2b0641
Kadrizzle <lanekade8@gmail.com>
2024-04-09 14:35:32 -0500

    Reservation work in backend

--- Selu383.SP24.Api/Features/Hotels/Reservation.cs
+++ Selu383.SP24.Api/Features/Hotels/Reservation.cs
@@ -4,10 +4,13 @@ namespace Selu383.SP24.Api.Features.Hotels
     public class Reservation
     {
         public int Id { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
         public string? Dates { get; set; }
+        public int ReservationNumber { get; set; }
         public int Guest { get; set; }
-        public Room? RoomReserved { get; set; }
         public Room? Room { get; set; }
+        public string? HotelName { get; set; }
         public int RoomId { get; set; }
         public User? User { get; set; }
         public int UserId { get; set; }
